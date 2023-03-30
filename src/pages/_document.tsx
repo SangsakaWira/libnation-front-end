f70bcf28eca6123039af75b3b84d229220dac6e7
Sangsaka Wira <sangsakawira@gmail.com>
2023-03-30 14:01:40 +0700

    add dashboard

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
-        <title>Libnation | Book Library App</title>
       </Head>
       <body>
         <Main />
